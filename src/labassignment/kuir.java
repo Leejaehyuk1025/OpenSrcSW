8ef7f0c697949c20f09058aec7c5ca02aa17fcf8
Leejaehyuk <tmanftpt32@gmail.com>
2021-04-07 14:38:28 +0900

    2021/04/07 5주차 과제

--- src/labassignment/kuir.java
+++ src/labassignment/kuir.java
@@ -58,8 +58,11 @@ public class kuir {
     	else if(args[0].equals("-k")) {
     		makeKeyword b = new makeKeyword(args[1]);
     	}
-    	else if(args[0].contentEquals("-i")) {
+    	else if(args[0].equals("-i")) {
     		indexer c = new indexer(args[1]);
+    	}
+    	else if(args[0].equals("-i")) {
+    		indexer s = new indexer(args[1]);
     	}
     	else {
     		System.out.println("올바르지 않은 명령");
